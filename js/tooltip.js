024534899e02913b806b1b7c33d48624eb757224
Bastoss <bastien.chantrel95@gmail.com>
2021-03-05 23:03:04 +0100

    rédaction rapport

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,36 +1,24 @@
+const tooltip = document.querySelector("#tooltip");
 
-const tooltip = document.querySelector('#tooltip')
+const container = document.querySelectorAll(".tooltip-hover ul");
+const list = document.querySelectorAll(".tooltip-hover li");
 
+list.forEach(el => {
+    el.addEventListener("mousemove", function (event) {
+        tooltip.style.display = "flex";
 
-const container = document.querySelectorAll('.tooltip-hover ul')
-const list = document.querySelectorAll('.tooltip-hover li')
+        tooltip.querySelector("h4").innerText = event.target.innerText;
+        tooltip.style.top = event.pageY + 15;
+        tooltip.style.left = event.pageX + 15;
+    });
+});
 
-list.forEach((el)=>{
-    
-    el.addEventListener('mousemove',function(event){
-        tooltip.style.display = 'flex'
+container.forEach(el => {
+    el.addEventListener("mouseleave", function (event) {
+        tooltip.style.display = "none";
+    });
 
-tooltip.querySelector('h4').innerText =event.target.innerText
-        tooltip.style.top = event.pageY + 15
-        tooltip.style.left = event.pageX + 15
-    }) 
-
-   
-    
-})
-
-
-
-
-
-container.forEach((el)=>{
-    el.addEventListener('mouseleave',function(event){
-        tooltip.style.display = 'none'
-    })
-
-    el.addEventListener('mouseenter',function(event){
-        tooltip.style.display = 'none'
-    })
-})
- 
-
+    el.addEventListener("mouseenter", function (event) {
+        tooltip.style.display = "none";
+    });
+});
